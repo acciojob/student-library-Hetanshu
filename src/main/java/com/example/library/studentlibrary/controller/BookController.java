2db7048fa68bc36ea5b6ebab1f9e521e45866214
Hetanshu <hetanshugupta@gmail.com>
2022-12-30 12:26:21 +0530

    Controller Commit

--- src/main/java/com/example/library/studentlibrary/controller/BookController.java
+++ src/main/java/com/example/library/studentlibrary/controller/BookController.java
@@ -12,20 +12,29 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 //Add required annotations
-
+@RestController
 public class BookController {
 
+    @Autowired
+    BookService bookService;
 
     //Write createBook API with required annotations
 
     //Add required annotations
+    @GetMapping("/book")
     public ResponseEntity getBooks(@RequestParam(value = "genre", required = false) String genre,
                                    @RequestParam(value = "available", required = false, defaultValue = "false") boolean available,
                                    @RequestParam(value = "author", required = false) String author){
 
-        List<Book> bookList = null; //find the elements of the list by yourself
+        List<Book> bookList = bookService.getBooks(genre,available,author); //find the elements of the list by yourself
 
         return new ResponseEntity<>(bookList, HttpStatus.OK);
 
+    }
+
+    @PostMapping("/book")
+    public ResponseEntity createBook(@RequestBody() Book book){
+        bookService.createBook(book);
+        return new ResponseEntity("Success",HttpStatus.CREATED);
     }
 }
